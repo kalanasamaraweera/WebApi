d7225ad3aef52f5819f35563cd1d8fbdf76f715f
mohammedibrahim.lj <mohammedibrahim.lj@gmail.com>
2016-02-01 20:51:21 -0800

    create student class

---
+++ ASPApi/StudentBasicDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPApi
+{
+    public class StudentBasicDetails
+    {
+        public int stuID { get; set; }
+        public string stuName { get; set; }
+        public int stuAge { get; set; }
+
+    }
+}

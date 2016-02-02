d1d5b7f504f218b629e91c4643f65221122cbf40
mohammedibrahim.lj <mohammedibrahim.lj@gmail.com>
2016-02-01 21:19:28 -0800

    controller created

--- ASPApi/StudentBasicDetails.cs
+++ ASPApi/StudentBasicDetails.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace ASPApi
 {
-    public class StudentBasicDetails
+    public class StudentBasicDetails:DbContext
+    {
+        public DbSet<studentTable> StudentDetails { get; set; }
+
+    }
+    public class studentTable
     {
         public int stuID { get; set; }
         public string stuName { get; set; }
         public int stuAge { get; set; }
-
     }
 }
